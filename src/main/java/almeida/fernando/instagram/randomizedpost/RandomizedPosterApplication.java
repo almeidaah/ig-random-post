bcbd8f06f7fad595f14cbd06c1e7486c9ad66504
Fernando Almeida <almeidascvn@gmail.com>
2019-08-30 16:01:29 +0100

    ig-randomizer-procfile

--- src/main/java/almeida/fernando/instagram/randomizedpost/RandomizedPosterApplication.java
+++ src/main/java/almeida/fernando/instagram/randomizedpost/RandomizedPosterApplication.java
@@ -2,6 +2,7 @@ package almeida.fernando.instagram.randomizedpost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
